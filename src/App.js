a2c829e6f0552b9cecb01d5e767157ef4776ce8e
Rupesh Gaudel Regmi <rupeshgaudel3@gmail.com>
2020-07-27 16:46:29 +0545

    Added React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+         Rupesh Gaudel Regmi
+         </h3>
+        <div className="social-container">
+          <p>@Social Links</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    href="https://www.facebook.com/profile.php?id=100005387297448"
+    className="facebook social"
+ >
+    <FontAwesomeIcon icon={faFacebook} size="2x" />
+ </a>
+ <a
+    href="https://www.linkedin.com/in/rupesh-gaudel-7a7863196/"
+    className="linkedin social"
+ >
+    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+ </a>
+       </div>
       </header>
     </div>
   );
