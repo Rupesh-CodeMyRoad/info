c27e4d0c2932ad9b90b7ca5eba067a62e0244e4a
Rupesh Gaudel Regmi <rupeshgaudel3@gmail.com>
2020-07-27 16:57:44 +0545

    Updates

---
+++ precache-manifest.de177b44cadf40525d8a4f8a302ee995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3787de7b0acb3dde00d63cbeb426da2e",
+    "url": "/info/index.html"
+  },
+  {
+    "revision": "a1da4901af30ff214b91",
+    "url": "/info/static/css/main.777efccd.chunk.css"
+  },
+  {
+    "revision": "8b886fd4986caf5522d4",
+    "url": "/info/static/js/2.5391ea76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/info/static/js/2.5391ea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1da4901af30ff214b91",
+    "url": "/info/static/js/main.d53530d9.chunk.js"
+  },
+  {
+    "revision": "9628ee48ccc5fef29d80",
+    "url": "/info/static/js/runtime-main.702bcbd8.js"
+  },
+  {
+    "revision": "04b12eef41dacf81310a18c03341fa7b",
+    "url": "/info/static/media/logo.04b12eef.svg"
+  }
+]);
